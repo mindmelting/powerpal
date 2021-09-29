72c43b6b84162b310c0dbd7696bc81d95538a113
Lawrence <lawrence.hunt@gmail.com>
2021-09-30 09:39:50 +1000

    feat: Add exception handling (#3)
    * feat: Add exception handling

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="powerpal",
-    version="0.1.0",
+    version="0.2.0",
     author="Lawrence Hunt",
     author_email="lawrence.hunt@gmail.com",
     url="https://github.com/mindmelting/powerpal",
