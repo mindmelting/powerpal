163696bd6ce4038e78399e3e69e1ed388c53c6e4
Lawrence Hunt <lawrence.hunt@gmail.com>
2021-10-05 10:53:09 +1100

    chore: Bump major version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mindmelting.powerpal",
-    version="0.2.1",
+    version="0.3.0",
     author="Lawrence Hunt",
     author_email="lawrence.hunt@gmail.com",
     url="https://github.com/mindmelting/powerpal",
